7af2150c49c32909685d916950eb6b1359822c03
Evandro Mateus <evandromateus06@gmail.com>
2021-09-10 20:54:59 -0300

    save animes on localstorage

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const animesEl = document.querySelector('#animes')
 const animeContainer = document.querySelector('#anime-container')
 const searchForm = document.querySelector('#search-form')
 const search = document.querySelector('#search')
-const APIURL = 'https://api.aniapi.com/v1/anime'
+const APIURL = 'https://api.aniapi.com/v1/anime/'
 const SEARCHURL = 'https://api.aniapi.com/v1/anime?title='
+
+const favLink = document.querySelector('#fav-animes')
+const watchLaterLink = document.querySelector('#watch-later-animes')
+const watchingLink = document.querySelector('#watching-animes')
+const watchedLink = document.querySelector('#watched-animes')
 
 getAnimes(APIURL)
 
@@ -68,19 +73,19 @@ function showAnimeInfo(anime) {
             <p class="description">${descriptions.en ? descriptions.en.replace('<br>', '') : 'Descrição indisponivel.'}</p>
             <div class="tools">
               <button class="tool">
-                <i class="bi bi-heart"></i>
+                <i id="fav" class="bi bi-heart"></i>
                 <span>Favoritar</span>
               </button>
               <button class="tool">
-                <i class="bi bi-clock"></i>
+                <i id="watch-later" class="bi bi-clock"></i>
                 <span>Ver depois</span>
               </button>
               <button class="tool">
-                <i class="bi bi-check-circle"></i>
+                <i id="watched" class="bi bi-check-circle"></i>
                 <span>Assistido</span>
               </button>
               <button class="tool">
-                <i class="bi bi-play-circle"></i>
+                <i id="watching" class="bi bi-play-circle"></i>
                 <span>Assistindo</span>
               </button>
             </div>
@@ -103,7 +108,87 @@ function showAnimeInfo(anime) {
     }
   })
 
+  const favIcon = document.querySelector('#fav')
+  favIcon.addEventListener('click', () => {
+    if (favIcon.classList.contains('bi-heart')) {
+      favIcon.className = 'bi bi-heart-fill'
+      addAnimeLS('favAnimes', anime.id)
+    } else {
+      favIcon.className = 'bi bi-heart'
+      removeAnimeLS('favAnimes', anime.id)
+    }
+  })
+
+  const watchLaterIcon = document.querySelector('#watch-later')
+  watchLaterIcon.addEventListener('click', () => {
+    if (watchLaterIcon.classList.contains('bi-clock')) {
+      watchLaterIcon.className = 'bi bi-clock-fill'
+      addAnimeLS('watchLaterAnimes', anime.id)
+    } else {
+      watchLaterIcon.className = 'bi bi-clock'
+      removeAnimeLS('watchLaterAnimes', anime.id)
+    }
+  })
+
+  const watchedIcon = document.querySelector('#watched')
+  watchedIcon.addEventListener('click', () => {
+    if (watchedIcon.classList.contains('bi-check-circle')) {
+      watchedIcon.className = 'bi bi-check-circle-fill'
+      addAnimeLS('watchedAnimes', anime.id)
+    } else {
+      watchedIcon.className = 'bi bi-check-circle'
+      removeAnimeLS('watchedAnimes', anime.id)
+    }
+  })
+
+  const watchingIcon = document.querySelector('#watching')
+  watchingIcon.addEventListener('click', () => {
+    if (watchingIcon.classList.contains('bi-play-circle')) {
+      watchingIcon.className = 'bi bi-play-circle-fill'
+      addAnimeLS('watchingAnimes', anime.id)
+    } else {
+      watchingIcon.className = 'bi bi-play-circle'
+      removeAnimeLS('watchingAnimes', anime.id)
+    }
+  })
+
 }
+
+function addAnimeLS(lsName, animeId) {
+  const animeIds = getAnimeIdsLS(lsName)
+
+  localStorage.setItem(lsName, JSON.stringify([...animeIds, animeId]))
+}
+
+function removeAnimeLS(lsName, animeId) {
+  const animeIds = getAnimeIdsLS(lsName)
+  
+  localStorage.setItem(lsName, JSON.stringify(animeIds.filter(id => id !== animeId)))
+}
+
+function getAnimeIdsLS(lsName) {
+  const animeIds = JSON.parse(localStorage.getItem(lsName))
+
+  return animeIds === null ? [] : animeIds
+}
+
+function getAnimesById(lsName) {
+  const ids = getAnimeIdsLS(lsName)
+  let animes = []
+
+  ids.forEach(async id => {
+    const resp = await fetch(APIURL + id)
+    const respData = await resp.json()
+    animes.push(respData.data)
+    showAnimes(animes)
+  })
+}
+
+favLink.addEventListener('click', () => getAnimesById('favAnimes'))
+watchLaterLink.addEventListener('click', () => getAnimesById('watchLaterAnimes'))
+watchingLink.addEventListener('click', () => getAnimesById('watchingAnimes'))
+watchedLink.addEventListener('click', () => getAnimesById('watchedAnimes'))
+
 
 searchForm.addEventListener('submit', e => {
   e.preventDefault()
